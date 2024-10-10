214385b4cdced5d784cb1ecb1a47cef9cdfe01b1
Arittro7 <arittro007@gmail.com>
2024-10-10 23:59:55 +0600

    Still stuck on the cookies

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -1,24 +1,24 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
 import BookingCard from "./BookingCard";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 import axios from "axios";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
   const [bookings, setBookings] = useState([]);
 
+  //
+
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
   useEffect(() => {
-    
-    axios.get(url, {withCredentials: true})
-    .then(res => {
-        setBookings(res.data);
-    })
-    // fetch(url)
-    //   .then((res) => res.json())
-    //   .then((data) => setBookings(data));
+    axios
+      .get(url, {withCredentials: true})
+      .then((res) => setBookings(res.data))
+      .catch((error) => console.error("Error fetching bookings:", error));
   }, [url]);
+  //
 
   const handleDeleteBooking = (id) => {
     // const proceed = confirm('Are You sure you want to delete');
@@ -40,12 +40,18 @@ const Bookings = () => {
             if (data.deletedCount > 0) {
               // alert('deleted successful');
               Swal.fire("Deleted!", "The booking has been deleted.", "success");
-              const remaining = bookings.filter((booking) => booking._id !== id);
+              const remaining = bookings.filter(
+                (booking) => booking._id !== id
+              );
               setBookings(remaining); // Update the state to remove the deleted booking
             }
           })
           .catch((error) => {
-            Swal.fire("Error!", "There was a problem deleting the booking.", "error");
+            Swal.fire(
+              "Error!",
+              "There was a problem deleting the booking.",
+              "error"
+            );
             console.error("Delete failed:", error);
           });
       } else if (result.isDenied) {
@@ -54,29 +60,28 @@ const Bookings = () => {
     });
   };
 
- const handleBookingConfirm = id => {
-  fetch(`http://localhost:5000/bookings/${id}`, {
-      method: 'PATCH',
+  const handleBookingConfirm = (id) => {
+    fetch(`http://localhost:5000/bookings/${id}`, {
+      method: "PATCH",
       headers: {
-          'content-type': 'application/json'
+        "content-type": "application/json",
       },
-      body: JSON.stringify({ status: 'confirm' })
-  })
-      .then(res => res.json())
-      .then(data => {
-          console.log(data);
-          if (data.modifiedCount > 0) {
-              // update state
-              const remaining = bookings.filter(booking => booking._id !== id);
-              const updated = bookings.find(booking => booking._id === id);
-              updated.status = 'confirm'
-              const newBookings = [updated, ...remaining];
-              setBookings(newBookings);
-          }
-      })
-}
-  
-  
+      body: JSON.stringify({ status: "confirm" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          // update state
+          const remaining = bookings.filter((booking) => booking._id !== id);
+          const updated = bookings.find((booking) => booking._id === id);
+          updated.status = "confirm";
+          const newBookings = [updated, ...remaining];
+          setBookings(newBookings);
+        }
+      });
+  };
+
   return (
     <div>
       <h2 className="text-3xl">
@@ -102,13 +107,12 @@ const Bookings = () => {
           </thead>
           <tbody>
             {bookings.map((booking) => (
-              <BookingCard 
-              key={booking._id} 
-              booking={booking}
-              handleDeleteBooking ={handleDeleteBooking}
-              handleBookingConfirm={handleBookingConfirm}
-              >
-              </BookingCard>
+              <BookingCard
+                key={booking._id}
+                booking={booking}
+                handleDeleteBooking={handleDeleteBooking}
+                handleBookingConfirm={handleBookingConfirm}
+              ></BookingCard>
             ))}
           </tbody>
         </table>
